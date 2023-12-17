517dd04f2d5071d6425727b04c97c863e15b0c10
Kabir Pal <kabirpal53@gmail.com>
2023-12-17 22:47:54 +0530

    Login component

--- feed-post-practice/src/App.tsx
+++ feed-post-practice/src/App.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './components/login';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='header'>Hello world</h1>
+    <div>
+      <Login></Login>
     </div>
-  );
+  )
 }
 
 export default App;
